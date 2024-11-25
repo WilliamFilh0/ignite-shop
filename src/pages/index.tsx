8ca624a93b16265068559de2575543e94f10ef5b
WilliamFilh0 <williamfilho8908@gmail.com>
2024-11-25 12:22:43 -0300

    perf: switch from getServerSideProps to getStaticProps for improved performance with SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 
 import { useKeenSlider } from "keen-slider/react";
@@ -6,10 +6,6 @@ import { useKeenSlider } from "keen-slider/react";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import { stripe } from "../lib/stripe";
-
-import camiseta1 from "../assets/camisetas/1.png";
-import camiseta2 from "../assets/camisetas/2.png";
-import camiseta3 from "../assets/camisetas/3.png";
 
 import "keen-slider/keen-slider.min.css";
 import Stripe from "stripe";
@@ -49,7 +45,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -71,5 +67,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: (60 * 60) / 2,
   };
 };
