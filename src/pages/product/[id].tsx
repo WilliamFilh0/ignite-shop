f217d986e12bb22a93bca21e6aa167d0265540ff
WilliamFilh0 <williamfilho8908@gmail.com>
2024-11-26 16:00:31 -0300

    feat: add static paths and static props for product retrieval

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,30 +1,77 @@
 import { useRouter } from "next/router";
+
 import {
   ProductContainer,
   ImageContainer,
   ProductDetails,
 } from "../../styles/pages/product";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { stripe } from "../../lib/stripe";
+import Stripe from "stripe";
+import Image from "next/image";
 
-export default function Product() {
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
   const { query } = useRouter();
 
   return (
     <ProductContainer>
-      <ImageContainer />
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-          reiciendis aliquid placeat earum, doloremque minima qui nisi! Aliquid
-          magni beatae atque? Eveniet tempore commodi veritatis totam voluptas
-          fugit sed quaerat!
-        </p>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+      revalidate: 60 * 60 * 1, // 1 hour
+    },
+  };
+};
